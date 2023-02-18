c15463e56340a6503921ce57f51895a92dec170c
V14dik <cherenkevich.vlad@gmail.com>
2023-02-18 21:42:41 +0300

    Add desctop version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, OutlinedInput, MenuItem, Select } from "@mui/material";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const seminars = ["First", "Second", "Third"];
+
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [seminar, setSeminar] = useState(seminars[0]);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(seminar);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="container">
+          <h1>Отправить заявку на участие в семинаре</h1>
+          <form className="form" onSubmit={submitHandler}>
+            <p>
+              Организаторы свяжутся с вами для подтверждения записи.
+              <br /> Участие в семинаре <ins>бесплатное</ins>.
+            </p>
+            <label htmlFor="name">Ваше имя:</label>
+            <OutlinedInput
+              id={"name"}
+              maxRows={1}
+              placeholder="Иванов Алексей"
+              color="secondary"
+              type="text"
+              value={userName}
+              sx={{ fontSize: "20px" }}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <label htmlFor="email">Контактный email:</label>
+            <OutlinedInput
+              maxRows={1}
+              placeholder="example@mail.com"
+              color="secondary"
+              type="text"
+              id={"email"}
+              value={email}
+              variant={"outlined"}
+              sx={{ fontSize: "20px" }}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label>Интересующий семинар:</label>
+            <Select
+              value={seminar}
+              color={"secondary"}
+              onChange={(e) => {
+                setSeminar(e.target.value);
+              }}
+              sx={{ fontSize: "20px" }}
+            >
+              {seminars.map((seminar, index) => (
+                <MenuItem
+                  key={seminar + index}
+                  value={seminar}
+                  color={"secondary"}
+                >
+                  {seminar}
+                </MenuItem>
+              ))}
+            </Select>
+            <div className="form-footer">
+              <span className="form-description">
+                Все поля обязательны для заполнения.
+                <br />
+                Отправляя заявку, вы соглашаетесь с договором публичной оферты и
+                политикой обработки данных.
+              </span>
+              <Button
+                type="submit"
+                variant="contained"
+                color="secondary"
+                size="large"
+              >
+                Отправить
+              </Button>
+            </div>
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
